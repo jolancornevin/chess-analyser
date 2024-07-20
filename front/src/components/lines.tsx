82e85736d7eedd4d31601d83a079d87f20143b79
Jolan Cornevin <jcornevin@work4labs.com>
2024-07-20 16:25:54 +0200

    refactor and fix line colors

--- front/src/components/lines.tsx
+++ front/src/components/lines.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 import { Line } from "../types/line";
 
 interface LinesProps {
@@ -6,52 +8,56 @@ interface LinesProps {
 }
 
 const LINE_HEIGHT = 60;
+let lineID = 0;
 
-export function Lines({ linesForNewPos, expectedLine }: LinesProps): JSX.Element {
+export function Lines({ linesForNewPos: _linesForNewPos, expectedLine: _expectedLine }: LinesProps): JSX.Element {
+    const linesForNewPos = useMemo(() => _linesForNewPos, [_linesForNewPos]);
+    const expectedLine = useMemo(() => _expectedLine, [_expectedLine]);
+
     return (
         <>
             {/* show expected line before the move */}
             Best Line:
             <div style={{ flex: 1, marginTop: 8, height: LINE_HEIGHT, border: "1px solid white" }}>
-                {expectedLine === undefined ? (
-                    <div style={{ height: `${LINE_HEIGHT}px`, textAlign: "left" }}></div>
-                ) : (
-                    <div style={{ height: `${LINE_HEIGHT}px`, textAlign: "left" }}>
-                        <span
-                            style={{
-                                backgroundColor: expectedLine.rawScore > 0 ? "white" : "#312e2",
-                                color: expectedLine.rawScore > 0 ? "black" : "white",
-                                fontWeight: "800",
-                            }}
-                        >
-                            {expectedLine.score} {expectedLine.rawScore} {expectedLine.rawScore > 0}
-                        </span>
-                        : {expectedLine.line}
-                    </div>
-                )}
+                <div style={{ height: `${LINE_HEIGHT}px`, textAlign: "left" }}>
+                    {expectedLine !== undefined && (
+                        <div style={{ height: `${LINE_HEIGHT}px`, textAlign: "left" }}>
+                            <span
+                                style={{
+                                    backgroundColor: expectedLine.rawScore > 0 ? "white" : "",
+                                    color: expectedLine.rawScore > 0 ? "black" : "white",
+                                    fontWeight: "800",
+                                }}
+                            >
+                                {expectedLine.score}
+                            </span>
+                            : {expectedLine.line}
+                        </div>
+                    )}
+                </div>
             </div>
             {/* show suggested line after the move */}
             New Lines:
             <div style={{ flex: 1, marginTop: 8, height: LINE_HEIGHT * 3, border: "1px solid white" }}>
                 {[0, 1, 2].map((i) => {
                     const line = linesForNewPos[i];
-                    if (line === undefined) {
-                        return <div key={i} style={{ height: `${LINE_HEIGHT}px`, textAlign: "left" }}></div>;
-                    }
 
                     return (
-                        <div key={i} style={{ height: `${LINE_HEIGHT}px`, textAlign: "left" }}>
-                            {/* TODO fix the colors of evals ???? */}
-                            <span
-                                style={{
-                                    backgroundColor: line.rawScore > 0 ? "white" : "#312e2",
-                                    color: line.rawScore > 0 ? "black" : "white",
-                                    fontWeight: "800",
-                                }}
-                            >
-                                {line.score}
-                            </span>
-                            : {line.line} {/* TODO make moves clickables */}
+                        <div key={lineID++} style={{ height: `${LINE_HEIGHT}px`, textAlign: "left" }}>
+                            {line !== undefined && (
+                                <>
+                                    <span
+                                        style={{
+                                            backgroundColor: line.rawScore > 0 ? "white" : "",
+                                            color: line.rawScore > 0 ? "black" : "white",
+                                            fontWeight: "800",
+                                        }}
+                                    >
+                                        {line.score}
+                                    </span>
+                                    : {line.line}
+                                </>
+                            )}
                         </div>
                     );
                 })}
